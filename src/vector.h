20813dd87cfe0828c4632f3d0032b1fe3c310548
leikareipa <drilledicecream@gmail.com>
2020-11-20 04:10:08 +0200

    vector: Add a few operator overloads

--- src/vector.h
+++ src/vector.h
@@ -65,22 +65,41 @@ struct vector3_s
         return;
     }
 
-    void operator*=(const Matrix44 &matrix)
+    vector3_s operator*(const real &x) const
+    {
+        return vector3_s((this->x * x), (this->y * x), (this->z * x));
+    }
+
+    vector3_s operator*(const Matrix44 &matrix) const
     {
         const decltype(this->x) x0 = ((matrix.elements[0] * this->x) + (matrix.elements[4] * this->y) + (matrix.elements[8] * this->z));
         const decltype(this->y) y0 = ((matrix.elements[1] * this->x) + (matrix.elements[5] * this->y) + (matrix.elements[9] * this->z));
         const decltype(this->z) z0 = ((matrix.elements[2] * this->x) + (matrix.elements[6] * this->y) + (matrix.elements[10] * this->z));
 
-        this->x = x0;
-        this->y = y0;
-        this->z = z0;
+        return vector3_s(x0, y0, z0);
+    }
+
+    vector3_s operator-(const vector3_s &other) const
+    {
+        return {(this->x - other.x),
+                (this->y - other.y),
+                (this->z - other.z)};
+    }
+
+    void operator*=(const Matrix44 &matrix)
+    {
+        *this = (*this * matrix);
 
         return;
     }
 
-    vector3_s operator*(const real &x) const
+    void operator*=(const vector3_s &v)
     {
-        return vector3_s((this->x * x), (this->y * x), (this->z * x));
+        this->x *= v.x;
+        this->y *= v.y;
+        this->z *= v.z;
+
+        return;
     }
 
     void operator+=(const vector3_s &other)
@@ -90,13 +109,6 @@ struct vector3_s
         this->z += other.z;
 
         return;
-    }
-
-    vector3_s operator-(const vector3_s &other) const
-    {
-        return {(this->x - other.x),
-                (this->y - other.y),
-                (this->z - other.z)};
     }
 };
 
